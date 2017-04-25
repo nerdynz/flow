619b700da0e0f74b2a36af03bb7659f6ec9d0121
Jaybeecave <josh@webbucket.io>
2017-04-25 15:06:48 +1200

    first commit

---
+++ flow.go
@@ -0,0 +1,31 @@
+package context
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/go-zoo/bone"
+	"github.com/nerdynz/datastore"
+)
+
+type Context struct {
+	W     http.ResponseWriter
+	Req   *http.Request
+	Store *datastore.Datastore
+}
+
+func New(w http.ResponseWriter, req *http.Request, store *datastore.Datastore) *Context {
+	return &Context{
+		W:     w,
+		Req:   req,
+		Store: store,
+	}
+}
+
+func (ctx *Context) URLParam(key string) string {
+	return bone.GetValue(ctx.Req, key)
+}
+
+func (ctx *Context) URLIntParam(key string) (int, error) {
+	return strconv.Atoi(ctx.URLParam(key))
+}
