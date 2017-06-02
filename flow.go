d17c6d18c8b3b258b9c9c81e641f93f8343d4521
Jaybeecave <josh@webbucket.io>
2017-06-02 20:32:49 +1200

    wrong pkg name

--- flow.go
+++ flow.go
@@ -29,3 +29,14 @@ func (ctx *Context) URLParam(key string) string {
 func (ctx *Context) URLIntParam(key string) (int, error) {
 	return strconv.Atoi(ctx.URLParam(key))
 }
+
+func (ctx *Context) Publish(cat string, data interface{}) error {
+	if ctx.Store.EventBus == nil {
+		return errors.New("Long Polling Event Manager not initialised")
+	}
+	return ctx.Store.EventBus.Publish(cat, data)
+}
+
+func (ctx *Context) JSON(status int, data interface{}) {
+	view.JSON(ctx.W, status, data)
+}
