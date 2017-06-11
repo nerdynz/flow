1c99f13b8802d5eb0ff2a202fbb730861f157151
Jaybeecave <josh@webbucket.io>
2017-06-11 14:50:44 +1200

    spa

--- flow.go
+++ flow.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"strconv"
 
+	"strings"
+
 	"github.com/go-zoo/bone"
 	"github.com/nerdynz/datastore"
 	"github.com/nerdynz/view"
@@ -44,18 +46,26 @@ func (ctx *Context) Publish(cat string, data interface{}) error {
 func (ctx *Context) JSON(status int, data interface{}) {
 	view.JSON(ctx.W, status, data)
 }
-func (ctx *Context) JS(status int, data interface{}) {
-	buf := bytes.NewBufferString(`<!DOCTYPE html>
+func (ctx *Context) SPA(status int, data *PageInfo) {
+	// logrus.Info(strings.ToLower(ctx.Req.Header.Get("Accept")))
+	if strings.Contains(strings.ToLower(ctx.Req.Header.Get("Accept")), "application/json") {
+		ctx.JSON(status, data)
+	} else {
+		path := ctx.Req.URL.Path
+		buf := bytes.NewBufferString(`<!DOCTYPE html>
 		<html>
+			<title>` + data.Title + " - " + data.SiteInfo.Tagline + " - " + data.SiteInfo.SiteName + `</title>
+			<link href="/css/main.css" rel="stylesheet" />
 			<script data-main="js/app" src="/js/require.js"></script>
-			<script>var cache = cache || {}; cache.data = `) // continues after render
-	json.NewEncoder(buf).Encode(data)
-	buf.Write([]byte("console.log('data', cache.data)"))
-	buf.Write([]byte("</script>"))
-	buf.Write([]byte("</html>"))
-	ctx.W.Header().Set("Content-Type", "text/html")
-	ctx.W.Header().Set("Content-Length", strconv.Itoa(len(buf.Bytes())))
-	ctx.W.Write(buf.Bytes())
+			<script>var cache = cache || {}; cache.root = '` + path + `'; cache.data = `) // continues after render
+		json.NewEncoder(buf).Encode(data)
+		buf.Write([]byte("console.log('data', cache.data)"))
+		buf.Write([]byte("</script>"))
+		buf.Write([]byte("</html>"))
+		ctx.W.Header().Set("Content-Type", "text/html")
+		ctx.W.Header().Set("Content-Length", strconv.Itoa(len(buf.Bytes())))
+		ctx.W.Write(buf.Bytes())
+	}
 }
 
 func (ctx *Context) Render(status int, buffer *bytes.Buffer) {
@@ -67,4 +77,14 @@ func (ctx *Context) RenderPDF(bytes []byte) {
 	ctx.W.Header().Set("Content-Type", "application/PDF")
 	ctx.W.Header().Set("Content-Length", strconv.Itoa(len(bytes)))
 	ctx.W.Write(bytes)
+}
+
+type SiteInfo struct {
+	Tagline  string
+	SiteName string
+}
+
+type PageInfo struct {
+	Title    string
+	SiteInfo *SiteInfo
 }
