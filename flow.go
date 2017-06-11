d2c38f387938aedb19b666ee46f930350c2e2295
Jaybeecave <josh@webbucket.io>
2017-06-11 16:57:37 +1200

    meta tags for spa

--- flow.go
+++ flow.go
@@ -47,20 +47,59 @@ func (ctx *Context) JSON(status int, data interface{}) {
 	view.JSON(ctx.W, status, data)
 }
 func (ctx *Context) SPA(status int, data *PageInfo) {
+	data.DocumentTitle = data.Title + " - " + data.SiteInfo.Tagline + " - " + data.SiteInfo.SiteName
 	// logrus.Info(strings.ToLower(ctx.Req.Header.Get("Accept")))
 	if strings.Contains(strings.ToLower(ctx.Req.Header.Get("Accept")), "application/json") {
 		ctx.JSON(status, data)
 	} else {
-		path := ctx.Req.URL.Path
+		url := ctx.Req.URL
 		buf := bytes.NewBufferString(`<!DOCTYPE html>
 		<html>
-			<title>` + data.Title + " - " + data.SiteInfo.Tagline + " - " + data.SiteInfo.SiteName + `</title>
+			<head>
+				<title>` + data.DocumentTitle + `</title>
+				<link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png">
+				<link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png">
+				<link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png">
+				<link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png">
+				<link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png">
+				<link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png">
+				<link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png">
+				<link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png">
+				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png">
+				<link rel="icon" type="image/png" sizes="192x192"  href="/icons/android-icon-192x192.png">
+				<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+				<link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png">
+				<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+				<link rel="manifest" href="/icons/manifest.json">
+				<meta name="msapplication-TileColor" content="#ffffff">
+				<meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png">
+				<meta name="theme-color" content="#ffffff">
+				
+				<meta name="description" content="` + data.Description + `">
+				<meta name="robots" content="index, follow">
+				<meta property="og:title" content="` + data.Title + `">
+
+				<meta property="og:type" content="website">
+				<meta property="og:description" content="` + data.Description + `">
+				<meta property="og:image" content="` + data.Image + `">
+				<meta property="og:url" content="` + url.RequestURI() + `">
+
+				<meta name="twitter:title" content="` + data.Title + `">
+				<meta name="twitter:card" content="summary">
+				<meta name="twitter:url" content="` + url.RequestURI() + `">
+				<meta name="twitter:image" content="` + data.Image + `">
+				<meta name="twitter:description" content="` + data.Description + `">
+			</head>
+			<body>
+			<div id="application"></div>
+			<script>var cache = cache || {}; cache.root = '` + url.Path + `'; cache.data = `) // continues after render
+
+		json.NewEncoder(buf).Encode(data)
+		buf.Write([]byte(`</script>
 			<link href="/css/main.css" rel="stylesheet" />
 			<script data-main="js/app" src="/js/require.js"></script>
-			<script>var cache = cache || {}; cache.root = '` + path + `'; cache.data = `) // continues after render
-		json.NewEncoder(buf).Encode(data)
-		buf.Write([]byte("console.log('data', cache.data)"))
-		buf.Write([]byte("</script>"))
+			</body>
+		`))
 		buf.Write([]byte("</html>"))
 		ctx.W.Header().Set("Content-Type", "text/html")
 		ctx.W.Header().Set("Content-Length", strconv.Itoa(len(buf.Bytes())))
@@ -81,10 +120,14 @@ func (ctx *Context) RenderPDF(bytes []byte) {
 
 type SiteInfo struct {
 	Tagline  string
-	SiteName string
+	Sitename string
 }
 
 type PageInfo struct {
-	Title    string
-	SiteInfo *SiteInfo
+	Title         string
+	Description   string
+	URL           string
+	Image         string
+	DocumentTitle string
+	SiteInfo      *SiteInfo
 }
