acc0f4889eb8e8e7a2653a1f73ec3379e7263abc
Jaybeecave <josh@webbucket.io>
2017-06-26 15:04:11 +1200

    Added renderer

--- flow.go
+++ flow.go
@@ -3,21 +3,29 @@ package flow
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
+	"fmt"
+	"html/template"
 	"net/http"
+	"net/url"
 	"strconv"
+	"time"
+
+	dat "gopkg.in/mgutz/dat.v1"
 
 	"strings"
 
 	"github.com/go-zoo/bone"
+	"github.com/jaybeecave/base/flash"
 	"github.com/nerdynz/datastore"
 	"github.com/nerdynz/view"
+	"github.com/unrolled/render"
 )
 
 type Context struct {
-	W     http.ResponseWriter
-	Req   *http.Request
-	Store *datastore.Datastore
+	W        http.ResponseWriter
+	Req      *http.Request
+	Store    *datastore.Datastore
+	Renderer *render.Render
 }
 
 func New(w http.ResponseWriter, req *http.Request, store *datastore.Datastore) *Context {
@@ -28,24 +36,45 @@ func New(w http.ResponseWriter, req *http.Request, store *datastore.Datastore) *
 	}
 }
 
+func NewWithRenderer(w http.ResponseWriter, req *http.Request, store *datastore.Datastore, renderer *render.Render) *Context {
+	return &Context{
+		W:        w,
+		Req:      req,
+		Store:    store,
+		Renderer: renderer,
+	}
+}
+
+func (ctx *Context) AddRenderer(renderer *render.Render) {
+	ctx.Renderer = renderer
+}
+
 func (ctx *Context) URLParam(key string) string {
-	return bone.GetValue(ctx.Req, key)
+	value := bone.GetValue(ctx.Req, key)
+	if value != "" {
+		newValue, err := url.QueryUnescape(value)
+		if err == nil {
+			value = strings.Replace(newValue, "%20", " ", -1)
+		}
+	}
+	return value
 }
 
 func (ctx *Context) URLIntParam(key string) (int, error) {
 	return strconv.Atoi(ctx.URLParam(key))
 }
 
-func (ctx *Context) Publish(cat string, data interface{}) error {
-	if ctx.Store.EventBus == nil {
-		return errors.New("Long Polling Event Manager not initialised")
-	}
-	return ctx.Store.EventBus.Publish(cat, data)
-}
+// func (ctx *Context) Publish(cat string, data interface{}) error {
+// 	if ctx.Store.EventBus == nil {
+// 		return errors.New("Long Polling Event Manager not initialised")
+// 	}
+// 	return ctx.Store.EventBus.Publish(cat, data)
+// }
 
 func (ctx *Context) JSON(status int, data interface{}) {
 	view.JSON(ctx.W, status, data)
 }
+
 func (ctx *Context) SPA(status int, data *PageInfo) {
 	data.DocumentTitle = data.Title + " - " + data.SiteInfo.Tagline + " - " + data.SiteInfo.Sitename
 	// logrus.Info(strings.ToLower(ctx.Req.Header.Get("Accept")))
@@ -57,22 +86,11 @@ func (ctx *Context) SPA(status int, data *PageInfo) {
 		<html>
 			<head>
 				<title>` + data.DocumentTitle + `</title>
-				<link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png">
-				<link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png">
-				<link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png">
-				<link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png">
-				<link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png">
-				<link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png">
-				<link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png">
-				<link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png">
-				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png">
-				<link rel="icon" type="image/png" sizes="192x192"  href="/icons/android-icon-192x192.png">
+				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
 				<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
-				<link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png">
 				<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
 				<link rel="manifest" href="/icons/manifest.json">
-				<meta name="msapplication-TileColor" content="#ffffff">
-				<meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png">
+				<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
 				<meta name="theme-color" content="#ffffff">
 				
 				<meta name="description" content="` + data.Description + `">
@@ -96,7 +114,7 @@ func (ctx *Context) SPA(status int, data *PageInfo) {
 
 		json.NewEncoder(buf).Encode(data)
 		buf.Write([]byte(`</script>
-			<link href="/css/main.css" rel="stylesheet" media="none" onload="if(media!='all')media='all'" />
+			<link href="/css/main.css" rel="stylesheet" />
 			<script data-main="js/app" src="/js/require.js"></script>
 			</body>
 		`))
@@ -130,4 +148,198 @@ type PageInfo struct {
 	Image         string
 	DocumentTitle string
 	SiteInfo      *SiteInfo
+}
+
+type ViewBucket struct {
+	renderer *render.Render
+	store    *datastore.Datastore
+	w        http.ResponseWriter
+	req      *http.Request
+	Data     map[string]interface{}
+}
+
+func NewBucket(ctx *Context) *ViewBucket {
+	viewBag := ViewBucket{}
+	viewBag.w = ctx.W
+	viewBag.req = ctx.Req
+	viewBag.store = ctx.Store
+	viewBag.renderer = ctx.Renderer
+	viewBag.Data = make(map[string]interface{})
+	viewBag.Add("Now", time.Now())
+	viewBag.Add("Year", time.Now().Year())
+
+	return &viewBag
+}
+
+func (viewBag *ViewBucket) Add(key string, value interface{}) {
+	viewBag.Data[key] = value
+	// spew.Dump(viewBag.data)
+}
+
+func (viewBag *ViewBucket) LoadNavItems() {
+	var navItems []*NavItem
+	err := viewBag.store.DB.
+		Select("title", "slug").
+		From("pages").
+		QueryStructs(&navItems)
+	if err != nil {
+		panic(err)
+	}
+	viewBag.Add("NavItems", navItems)
+}
+
+func (viewBag *ViewBucket) HTML(status int, templateName string) {
+
+	// automatically show the flash message if it exists
+	msg, _ := flash.GetFlash(viewBag.w, viewBag.req, "InfoMessage")
+	viewBag.Add("InfoMessage", msg) // if its blank it can be blank but atleast it will exist
+
+	viewBag.renderer.HTML(viewBag.w, status, templateName, viewBag.Data)
+}
+
+func (viewBag *ViewBucket) Text(status int, text string) {
+	viewBag.renderer.Text(viewBag.w, status, text)
+}
+
+var TemplateFunctions = template.FuncMap{
+	"javascript": javascriptTag,
+	"stylesheet": stylesheetTag,
+	"image":      imageTag,
+	"imagepath":  imagePath,
+	"content":    content,
+	"htmlblock":  htmlblock,
+	"navigation": navigation,
+	"link":       link,
+	"title":      title,
+
+	"isBlank":    isBlank,
+	"isNotBlank": isNotBlank,
+	"formatDate": formatDate,
+	"htmlsafe":   htmlSafe,
+	"gt":         greaterThan,
+}
+
+func greaterThan(num int, amt int) bool {
+	return num > amt
+}
+
+func content(contents ...string) template.HTML {
+	var str string
+	for _, content := range contents {
+		str += "<div class='standard'>" + content + "</standard>"
+	}
+	return template.HTML(str)
+}
+
+func javascriptTag(names ...string) template.HTML {
+	var str string
+	for _, name := range names {
+		str += "<script src='/js/" + name + ".js' type='text/javascript'></script>"
+	}
+	return template.HTML(str)
+}
+
+func stylesheetTag(names ...string) template.HTML {
+	var str string
+	for _, name := range names {
+		str += "<link rel='stylesheet' href='/css/" + name + ".css' type='text/css' media='screen'  />\n"
+	}
+	return template.HTML(str)
+}
+
+func imagePath(name string) string {
+	return "/images/" + name
+}
+
+func imageTag(name string, class string) template.HTML {
+	return template.HTML("<image src='" + imagePath(name) + "' class='" + class + "' />")
+}
+
+func htmlSafe(str string) template.HTML {
+	return template.HTML(str)
+}
+
+func htmlblock(page *Page, code string) template.HTML {
+	html := "<div class='textblock editable' "
+	html += " data-textblock='page-" + strconv.FormatInt(page.PageID, 10) + "-" + code + "'"
+	html += " data-placeholder='#{placeholder}'> "
+	html += getHTMLFromTextblock(page, code)
+	html += "</div>"
+	return template.HTML(html)
+}
+
+func link(text string, link string, viewBag *ViewBucket) template.HTML {
+	class := "link link-" + strings.ToLower(text)
+	if strings.ToLower(link) == viewBag.req.URL.Path {
+		class += " active"
+	}
+	return template.HTML(fmt.Sprintf(`<a class="%v" href="%v">%v</a>`, class, link, text))
+}
+
+func title(text string) string {
+	return strings.Title(text)
+}
+
+func navigation(viewBag *ViewBucket) template.HTML {
+	html := ""
+	if viewBag.Data["NavItems"] != nil {
+		navItems := viewBag.Data["NavItems"].([]*NavItem)
+		html = "<nav class='main-nav closed'>"
+		for _, navItem := range navItems {
+			html += "<a href='/" + navItem.Slug + "'>" + navItem.Title + "</a>"
+		}
+		html += "</nav>"
+	}
+	return template.HTML(html)
+}
+
+func isBlank(str string) bool {
+	return str == ""
+}
+
+func isNotBlank(str string) bool {
+	return !isBlank(str)
+}
+
+type Page struct {
+	PageID     int64        `db:"page_id"`
+	Title      string       `db:"title"`
+	Body       string       `db:"body"`
+	Slug       string       `db:"slug"`
+	Template   string       `db:"template"`
+	CreatedAt  dat.NullTime `db:"created_at"`
+	UpdatedAt  dat.NullTime `db:"updated_at"`
+	Textblocks []*Textblock
+}
+
+type NavItem struct {
+	Title string `db:"title"`
+	Slug  string `db:"slug"`
+}
+
+func (navItem *NavItem) getURL() string {
+	return ""
+}
+
+type Textblock struct {
+	TextblockID int64        `db:"textblock_id"`
+	Code        string       `db:"code"`
+	Body        string       `db:"body"`
+	CreatedAt   dat.NullTime `db:"created_at"`
+	UpdatedAt   dat.NullTime `db:"updated_at"`
+	PageID      int64        `db:"page_id"`
+}
+
+func getHTMLFromTextblock(page *Page, code string) string {
+	var body string
+	for _, tb := range page.Textblocks {
+		if tb.Code == code {
+			body = tb.Body
+		}
+	}
+	return body
+}
+
+func formatDate(time time.Time, layout string) string {
+	return time.Format(layout)
 }
